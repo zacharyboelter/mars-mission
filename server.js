f13e440f8e79ca34c3324ee3dda96bb6723123e7
Zachary Boelter <zboelter@gmail.com>
2021-09-23 08:49:47 -0500

    final commit?

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/mars', (req, res) => {
     res.render("index.ejs", {allMars: marsMissions});
 });
 
+app.get('/mars/:missionIndex', (req, res) => {
+    res.render("show.ejs", {
+        mars: marsMissions[req.params.missionIndex]
+    });
+});
 
 app.listen(port, () => {
     console.log('houston, we have a port', port);
